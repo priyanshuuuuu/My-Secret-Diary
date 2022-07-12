2c174ebc7e206230c2293cf38be9a96f259d1c19
Priyanshu <86430218+priyanshuuuuu@users.noreply.github.com>
2022-07-12 11:11:29 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link =mysqli_connect("localhost","u613965840_himanshuDiary","database123WebSD","u613965840_SecretDiary");
+    $link =mysqli_connect("localhost","u613965840_priyanshuDiary","database123WebSD","u613965840_SecretDiary");
             if(mysqli_connect_error()){
                 die ("Database connection error");
             }
